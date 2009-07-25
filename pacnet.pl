2de43d7f34927400f21856637c81436db7d8790f
tadzik <tadzikes@gmail.com>
2009-07-25 21:25:59 +0200

    Added search option

--- pacnet.pl
+++ pacnet.pl
@@ -1,3 +1,4 @@
+#!/usr/bin/perl
 use JSON::XS;
 use strict;
 use warnings;
@@ -8,22 +9,40 @@ my $YELLOW = "\e[1;33m";
 my $CYAN = "\e[1;36m";
 my $NORMAL = "\e[0m";
 my $BLUE = "\e[1;34m";
+my $packages; #here be database
 
-unless(-e "pacdb") {
-	print "Downloading package database...\n";
+sub getdb() {
+	print ":: Downloading package database...\n";
 	if(system("wget http://pacnet.karbownicki.com/api/packages/ -O pacdb")) {#Why do I use wget instead of LWP? Just beacuse everyone has wget, and not everyone has LWP.
-		die("Could not obtain package database, exiting\n");									#You think it's a bad practice? Well, fuck you.
+		die(":: Could not obtain package database, exiting\n");
+  }
+}
+
+sub readdb() {
+	getdb unless(-e "pacdb");
+	
+	open(my $db, "<", "pacdb") or die ("Could not open the package database, exiting\n");
+	my $json;
+	$json .= $_	while(<$db>);
+	$packages = decode_json($json);
+}
+
+sub printall() {
+	for(@{$packages}) {
+		print "$CYAN$_->{category__name}$NORMAL/$GREEN$_->{name}$NORMAL $BLUE($_->{version})$NORMAL\n\t$_->{description}\n\n";
 	}
 }
 
-open(my $db, "<", "pacdb") or die ("Could not open the package database, exiting\n");
-my $json;
-while(<$db>) {
-	$json .= $_;
-}
+#main
+readdb();
+my $args = @ARGV;
 
-my $packages = decode_json($json);
-
-foreach(@{$packages}) {
-	print "$CYAN$_->{category__name}$NORMAL/$GREEN$_->{name}$NORMAL $BLUE($_->{version})$NORMAL\n\t$_->{description}\n\n";
+if(!$args) {
+	printall;
+} else {
+	for(@{$packages}) {
+		if($_->{name} =~ ${ARGV[0]}) {
+			print "$CYAN$_->{category__name}$NORMAL/$GREEN$_->{name}$NORMAL $BLUE($_->{version})$NORMAL\n\t$_->{description}\n\n";
+		}
+	}
 }
